50a44dc22a66bb7177509c93f074cdfdcd644929
Beau Ayres <beau@domesticcat.com.au>
2017-11-02 00:52:41 +1100

    added username and password reducers

--- manager/src/components/LoginForm.js
+++ manager/src/components/LoginForm.js
@@ -1,37 +1,58 @@
 /* @flow */
 
 import React, { Component } from 'react';
-import {
-  View,
-  Text,
-} from 'react-native';
+import { connect } from 'react-redux';
 import { Card, CardSection, Input, Button } from './common';
+import { emailChanged, passwordChanged } from './../actions';
 
-export default class LoginForm extends Component {
+class LoginForm extends Component {
+  onEmailChange(text) {
+    this.props.emailChanged(text);
+  }
+
+  onPasswordChange(text) {
+    this.props.passwordChanged(text);
+  }
+
   render() {
+    const { email, password } = this.props;
     return (
       <Card>
         <CardSection>
           <Input
             label="Email"
             placeholder="email@gmail.com"
+            onChangeText={this.onEmailChange.bind(this)}
+            value={email}
           />
-        </CardSection>
-        
-        <CardSection>
-          <Input
-            SecureTextEntry
-            label="Password"
-            placeholder="password"
-           />
         </CardSection>
 
         <CardSection>
-          <Button>
-            Login
-          </Button>
+          <Input
+            secureTextEntry
+            label="Password"
+            placeholder="password"
+            onChangeText={this.onPasswordChange.bind(this)}
+            value={password}
+          />
+        </CardSection>
+
+        <CardSection>
+          <Button>Login</Button>
         </CardSection>
       </Card>
     );
   }
 }
+
+const mapStateToProps = state => {
+  const { email, password } = state.auth;
+  return {
+    email,
+    password
+  };
+};
+
+export default connect(mapStateToProps, { emailChanged, passwordChanged })(
+  LoginForm
+);
