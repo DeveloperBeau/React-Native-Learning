3f387afddf0b738355e948fd0803ecd23f9be1eb
Beau Ayres <beau@domesticcat.com.au>
2017-11-05 15:56:24 +1100

    added in login using redux thunk

--- manager/src/App.js
+++ manager/src/App.js
@@ -1,28 +1,29 @@
 /* @flow */
-import React, { Component } from "react";
-import { View, Text } from "react-native";
-import { Provider } from "react-redux";
-import { createStore } from "redux";
-import firebase from "firebase";
-import reducers from "./reducers";
-import LoginForm from "./components/LoginForm";
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import firebase from 'firebase';
+import ReduxThunk from 'redux-thunk';
+import reducers from './reducers';
+import LoginForm from './components/LoginForm';
 
 export default class App extends Component {
   componentWillMount() {
     const config = {
-      apiKey: "AIzaSyCO1T2AlhhtY9UTabGNgJ55En-09mA5sjo",
-      authDomain: "manager-895ea.firebaseapp.com",
-      databaseURL: "https://manager-895ea.firebaseio.com",
-      projectId: "manager-895ea",
-      storageBucket: "",
-      messagingSenderId: "433158965003"
+      apiKey: 'AIzaSyCO1T2AlhhtY9UTabGNgJ55En-09mA5sjo',
+      authDomain: 'manager-895ea.firebaseapp.com',
+      databaseURL: 'https://manager-895ea.firebaseio.com',
+      projectId: 'manager-895ea',
+      storageBucket: '',
+      messagingSenderId: '433158965003'
     };
     firebase.initializeApp(config);
   }
 
   render() {
+    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
     return (
-      <Provider store={createStore(reducers)}>
+      <Provider store={store}>
         <LoginForm />
       </Provider>
     );
--- manager/src/components/LoginForm.js
+++ manager/src/components/LoginForm.js
@@ -3,7 +3,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Card, CardSection, Input, Button } from './common';
-import { emailChanged, passwordChanged } from './../actions';
+import { emailChanged, passwordChanged, loginUser } from './../actions';
 
 class LoginForm extends Component {
   onEmailChange(text) {
@@ -12,6 +12,12 @@ class LoginForm extends Component {
 
   onPasswordChange(text) {
     this.props.passwordChanged(text);
+  }
+
+  onButtonPress() {
+    const { email, password } = this.props;
+
+    this.props.loginUser({ email, password });
   }
 
   render() {
@@ -38,7 +44,7 @@ class LoginForm extends Component {
         </CardSection>
 
         <CardSection>
-          <Button>Login</Button>
+          <Button onPress={this.onButtonPress.bind(this)}>Login</Button>
         </CardSection>
       </Card>
     );
@@ -53,6 +59,8 @@ const mapStateToProps = state => {
   };
 };
 
-export default connect(mapStateToProps, { emailChanged, passwordChanged })(
-  LoginForm
-);
+export default connect(mapStateToProps, {
+  emailChanged,
+  passwordChanged,
+  loginUser
+})(LoginForm);
