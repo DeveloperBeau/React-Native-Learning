3b54245f92b32b923b1c9aa14c2e869a87e15190
Beau Ayres <beau@domesticcat.com.au>
2017-11-05 16:54:27 +1100

    created router

--- manager/src/App.js
+++ manager/src/App.js
@@ -5,7 +5,7 @@ import { createStore, applyMiddleware } from 'redux';
 import firebase from 'firebase';
 import ReduxThunk from 'redux-thunk';
 import reducers from './reducers';
-import LoginForm from './components/LoginForm';
+import Router from './Router';
 
 export default class App extends Component {
   componentWillMount() {
@@ -24,7 +24,7 @@ export default class App extends Component {
     const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
     return (
       <Provider store={store}>
-        <LoginForm />
+        <Router />
       </Provider>
     );
   }
